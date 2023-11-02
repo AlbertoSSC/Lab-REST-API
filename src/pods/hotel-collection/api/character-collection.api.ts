1caaa74d91024be688bdff0ff78a85a961984a92
AlbertoSSC <albertossc@gmail.com>
2023-11-02 17:07:14 +0100

    refactor env

--- src/pods/hotel-collection/api/character-collection.api.ts
+++ src/pods/hotel-collection/api/character-collection.api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-import { CharacterEntityApi } from '../../../common/models/character.api-model';
+import { CharacterEntityApi } from 'common';
 
 export const getCharacterCollection = async (): Promise<
   CharacterEntityApi[]
 > => {
-  const response = await axios.get(process.env.API_BASE_URL +'/characters/');
+  const response = await axios.get(process.env.API_BASE_URL + '/characters/');
   return response.data;
 };
--- src/pods/hotel/api/character.api.ts
+++ src/pods/hotel/api/character.api.ts
@@ -5,7 +5,7 @@ import { CharacterEntityApi } from 'common/models';
 export const getCharacter = async (i: number): Promise<CharacterEntityApi> => {
   const id = i.toString();
   const response = await axios.get(
-    `http://localhost:3000/characters/${id}`
+    process.env.API_BASE_URL + `/characters/${id}`
   );
   return response.data;
 };
